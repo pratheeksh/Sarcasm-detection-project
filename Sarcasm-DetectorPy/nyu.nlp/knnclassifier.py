f65e0e36e79e0b1a156199119701be79a0e539d2
pratheeksh <pks329@nyu.edu>
2016-05-07 15:20:43 -0400

    returning instead of printing

--- Sarcasm-DetectorPy/nyu.nlp/knnclassifier.py
+++ Sarcasm-DetectorPy/nyu.nlp/knnclassifier.py
@@ -2,28 +2,43 @@ import csv
 import statistics
 import numpy as np
 from sklearn.neighbors import KNeighborsClassifier
+import sys
 import extractsarcastic
+
+reload(sys)
+sys.setdefaultencoding('utf8')
+
 class knnClassifier():
 ### Define specific features
-    def extract_features_train(self,traindata):
+    def extract_features_train(self,traindata,testdata):
         train = []
         target = []
+        test = []
+        count =0
+        print len(testdata)
+        for data in testdata:
+        #    print count
+            count+=1
+            test.append(self.extract_features_sentence(data['Text']))
         for data in traindata:
             train.append(self.extract_features_sentence(data['Text']))
             target.append(round(float(data['Score'])*100))
-        self.classify(train,target)
+        return train,target,test
     def extract_features_sentence(self,text):
+        print text
         return [ text.count('!'),
                  text.count('?'),
                  len(text),
                 text.count('\"')]+extractsarcastic.identify_sentiment(text)
-    def classify(self,features,target):
+    def classify(self,features,target,test):
         print features,target
-        neigh = KNeighborsClassifier(n_neighbors=5)
+        neigh = KNeighborsClassifier(n_neighbors=10)
         neigh.fit(np.array(features), np.array(target))
-        vector = self.extract_features_sentence("I love exams!! Can't wait for more.")
-
-        print neigh.predict([vector])
+        print test
+        for sent in test:
+            vector = self.extract_features_sentence(sent)
+            
+            print sent,neigh.predict([vector])
     def generate_dataframe(self,filename):
         with open(filename,"rb") as csvfile:
             reader = csv.DictReader(csvfile)
