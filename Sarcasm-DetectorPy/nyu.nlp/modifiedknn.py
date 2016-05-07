25d141c2bccc10a08dfe4a6d081fe67479bbfedf
reachbp <bharathipriyaa@172-16-176-118.DYNAPOOL.NYU.EDU>
2016-05-07 15:43:20 -0400

    Modified Knn

--- Sarcasm-DetectorPy/nyu.nlp/modifiedknn.py
+++ Sarcasm-DetectorPy/nyu.nlp/modifiedknn.py
@@ -5,14 +5,15 @@ import knnclassifier as knn
 from collections import Counter
 
 class ModifiedKNN():
-
+    K=10
     def count_labels(self,traintarget):
         arr=np.array(traintarget)
         arr.sort()
-        count_labels=np.array(10)
-
+        count_labels=Counter(arr)
         print count_labels
     def predict_each_test(self,trainvectors,traintarget,testvectors):
+        countlabels=self.count_labels(traintarget)
+        test_predictions=[]
         for each_test_vector in testvectors:
             dist=np.array(traintarget.len)
             for each_train_vector in trainvectors:
@@ -21,7 +22,11 @@ class ModifiedKNN():
             dict={'Distance': dist,'Scores' : traintarget}
             df=pd.DataFrame(data=dict)
             df=df.sort_values(by='Distance')
-
+            prediction=0
+            for i in range(self.K):
+                label_i=df[i]['Scores']
+                prediction=prediction+ countlabels[label_i]/traintarget.len
+            prediction=prediction/self.K
 
     def calculate_euclidean(self, f1,f2):
         return np.dot(f1,f2)
