a0f206b476f45dc35f456ca27993b4e023d5cab9
reachbp <bharathipriyaa@172-16-176-118.DYNAPOOL.NYU.EDU>
2016-05-07 16:28:09 -0400

    Modified and Sarcastic

--- Sarcasm-DetectorPy/nyu.nlp/modifiedknn.py
+++ Sarcasm-DetectorPy/nyu.nlp/modifiedknn.py
@@ -11,33 +11,36 @@ class ModifiedKNN():
         arr.sort()
         count_labels=Counter(arr)
         print count_labels
+    # predict
     def predict_test(self,trainvectors,traintarget,testvectors):
         countlabels=self.count_labels(traintarget)
         test_predictions=[]
         for each_test_vector in testvectors:
-            dist=np.array(traintarget.len)
+            dist=[]
             for each_train_vector in trainvectors:
                 distance=self.calculate_euclidean(each_test_vector,each_train_vector)
                 dist.append(distance)
+
             dict={'Distance': dist,'Scores' : traintarget}
             df=pd.DataFrame(data=dict)
-            df=df.sort_values(by='Distance')
+            df=df.sort_values(by='Distance',ascending=True)
             prediction=0
+            print df[1]['Scores']
+            """"
             for i in range(self.K):
+
                 label_i=df[i]['Scores']
                 prediction=prediction+ countlabels[label_i]/traintarget.len
             prediction=prediction/self.K
             print "Sentence {} is predicted with Score {}".format(each_test_vector,prediction)
             test_predictions.append(prediction)
+            """
         return test_predictions
     def calculate_euclidean(self, f1,f2):
         return np.dot(f1,f2)
 
-    def test(self):
-        arr=[1,2,3,1,1,1]
-        self.count_labels(arr)
+
 def main():
-    print ("In main")
-    ModifiedKNN().test()
+    print "In main"
 if __name__ == '__main__':
     main()
