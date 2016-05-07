32db82f39e952974b1b36abad93dcee45477fe3d
pratheeksh <pks329@nyu.edu>
2016-05-07 13:22:35 -0400

    changed to accept sentence and return a list including polarity and subjectivity

--- Sarcasm-DetectorPy/nyu.nlp/extractsarcastic.py
+++ Sarcasm-DetectorPy/nyu.nlp/extractsarcastic.py
@@ -3,33 +3,16 @@ import numpy as np
 from textblob import TextBlob
 
 
-def extract_negative_reviews(filename):
-    df=pd.read_csv(filename)
-    neg_df=df[df['stars'] < 3]
-    identify_positive_sentiment(neg_df)
+def identify_sentiment(text):
+    blob = TextBlob(text)
+    sentimentscore = 0
+    try:
+        return list(blob.sentiment)
 
-def identify_positive_sentiment(neg_df):
-    n = neg_df.shape[0]
-    i = 0
-    review_arr = np.zeros(n, dtype=np.float32)
-    for eachrow in neg_df.itertuples():
-        text = eachrow[3]
-        blob = TextBlob(text)
-        sentimentscore = 0
-        try:
-            for sentence in blob.sentences:
-                sentimentscore = sentimentscore + sentence.sentiment.polarity
-            sentimentscore = sentimentscore/blob.sentences.__sizeof__()
-            print(text,sentimentscore)
-        except (RuntimeError, TypeError, NameError,UnicodeDecodeError):
-            pass
-        review_arr[0] = sentimentscore
-        i=i+1
-    neg_df.insert(0,'Score',review_arr)
-    neg_df.to_csv("ReviewsWithScore.csv")
+    except (RuntimeError, TypeError, NameError,UnicodeDecodeError):
+        pass
 
 def main():
-    filename="/Users/bharathipriyaa/Desktop/NLP-Project/Sarcasm-DetectorPy/data/test.csv"
-    extract_negative_reviews(filename)
+    print identify_sentiment("Simple is better than complex")
 if __name__ == '__main__':
     main()
