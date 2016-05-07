0dcdb14af6ad4b922e41c16f5ae8df6d91f1af4f
pratheeksh <pks329@nyu.edu>
2016-05-07 15:35:31 -0400

    updated knn

--- Sarcasm-DetectorPy/nyu.nlp/knnclassifier.py
+++ Sarcasm-DetectorPy/nyu.nlp/knnclassifier.py
@@ -15,30 +15,27 @@ class knnClassifier():
         target = []
         test = []
         count =0
-        print len(testdata)
-        for data in testdata:
-        #    print count
-            count+=1
-            test.append(self.extract_features_sentence(data['Text']))
         for data in traindata:
             train.append(self.extract_features_sentence(data['Text']))
             target.append(round(float(data['Score'])*100))
-        return train,target,test
+        return train,target,testdata
     def extract_features_sentence(self,text):
         print text
         return [ text.count('!'),
                  text.count('?'),
                  len(text),
-                text.count('\"')]+extractsarcastic.identify_sentiment(text)
+                text.count('\"')]+extractsarcastic.identify_sentiment(str(text))
     def classify(self,features,target,test):
-        print features,target
+        #print features,target
+        output = []
         neigh = KNeighborsClassifier(n_neighbors=10)
         neigh.fit(np.array(features), np.array(target))
-        print test
         for sent in test:
-            vector = self.extract_features_sentence(sent)
             
-            print sent,neigh.predict([vector])
+            vector = self.extract_features_sentence(sent['Text'])
+             
+            output.append([sent,neigh.predict([vector])])
+        return output
     def generate_dataframe(self,filename):
         with open(filename,"rb") as csvfile:
             reader = csv.DictReader(csvfile)
