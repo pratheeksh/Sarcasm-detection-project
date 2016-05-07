fb8b7ee4b09e596a764c7264f642eef728ac684f
pratheeksh <pks329@nyu.edu>
2016-05-07 11:50:53 -0400

    working knn

--- Sarcasm-DetectorPy/nyu.nlp/knnclassifier.py
+++ Sarcasm-DetectorPy/nyu.nlp/knnclassifier.py
@@ -1,19 +1,46 @@
-
+import csv
+import statistics
+import numpy as np
 from sklearn.neighbors import KNeighborsClassifier
 class Classifier():
 ### Define specific features
-    def extract_features(self,text):
-        res = []
-        for sent in text:
-            res.append(self.extract_features_sentence(sent))
-        
+    def extract_features_train(self):
+        train = []
+        target = []
+        train_data = self.generate_dataframe("../data/amazon.csv")
+        for data in train_data:
+            train.append(self.extract_features_sentence(data['text']))
+            target.append(round(float(data['score'])))
+        self.classify(train,target)
     def extract_features_sentence(self,text):
-        print text
         return [ text.count('!'),
                  text.count('?'),
-                 text.length,
+                 len(text),
                 text.count('\"')]
     def classify(self,features,target):
+        print features,target
         neigh = KNeighborsClassifier(n_neighbors=3)
-        neigh.fit(features, target)
-        print neigh.predict(
+        neigh.fit(np.array(features), np.array(target))
+        vector = self.extract_features_sentence("I love exams!! Can't wait for more.")
+
+        print neigh.predict([vector])
+    def generate_dataframe(self,filename):
+        with open(filename,"rb") as csvfile:
+            reader = csv.DictReader(csvfile)
+            train_data = []
+            for row in reader:
+                res = {}
+                text = row['Text']
+                b, c, d, e = float(row['MT1']), float(row['MT2']), float(row['MT3']), float(row['SASI'])
+                avg_score = statistics.mean([b,c,d,e])
+                res['text'] = text
+                res['score'] = avg_score
+                train_data.append(res)
+        print train_data
+        return train_data
+
+def main():
+    sc=Classifier()
+    sc.extract_features_train()
+if __name__ == '__main__':
+    main()
